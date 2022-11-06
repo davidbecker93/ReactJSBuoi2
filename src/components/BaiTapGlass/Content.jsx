b9a432e148fce547a58b4b27b0a5ecaccb684b7b
davidbecker93 <trongvanle93@gmail.com>
2022-11-06 11:12:16 +0700

    frist upload

--- src/components/BaiTapGlass/Content.jsx
+++ src/components/BaiTapGlass/Content.jsx
@@ -2,31 +2,21 @@ import React, { Component } from 'react'
 
 export default class Body extends Component {
     render() {
-        let { data } = this.props;
+        let { url, name, desc } = this.props;
         return (
             <div>
                 <header>TRY GLASSES APP ONLINE</header>
                 <div className="container">
                     <div className="row mt-3 align-items-center">
                         <div className="col-6 text-center">
-                            <img
-                                src="./glassesImage/model.jpg"
-                                alt="..."
-                                width={300}
-                                height={350}
-                            />
+                            <img src="./glassesImage/model.jpg" alt="..." width={300} height={350} />
                         </div>
-                        <div id="glassesModel" className="col-6 text-center">
-                            <img
-                                src="./glassesImage/model.jpg"
-                                alt="..."
-                                width={300}
-                                height={350}
-                            />
-                            <img className="glasses" src={data.url} alt="..." />
-                            <div className="glassesInfo px-2">
-                                <h3 className="mb-2">{data.name}</h3>
-                                <p>{data.desc}</p>
+                        <div id="model" className="col-6 text-center">
+                            <img src="./glassesImage/model.jpg" alt="..." width={300} height={350} />
+                            <img className="glasses" src={url} alt="..." />
+                            <div className="Info px-2">
+                                <h4 className="mb-1">{name}</h4>
+                                <p>{desc}</p>
                             </div>
                         </div>
                     </div>
